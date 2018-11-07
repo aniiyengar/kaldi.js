919697b517ef96acc0839c562271035ef61020f7
Aniruddh Iyengar <aniiyengar@berkeley.edu>
2018-11-06 17:25:46 -0800

    Add python server thing

---
+++ src/js/server.py
@@ -0,0 +1,14 @@
+
+import SimpleHTTPServer, SocketServer
+
+PORT=8000
+
+class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+	pass
+
+Handler.extensions_map['.wasm'] = 'application/wasm'
+
+httpd = SocketServer.TCPServer(('', PORT), Handler)
+
+print('Serving at port ' + str(PORT))
+httpd.serve_forever()
